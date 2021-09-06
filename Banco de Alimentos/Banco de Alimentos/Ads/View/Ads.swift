4ce78d9d30bea1e995c1059098f852924d609a49
dav <user192057@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2021-09-06 12:59:20 -0400

    ads added

--- Banco de Alimentos/Banco de Alimentos/Ads/View/Ads.swift
+++ Banco de Alimentos/Banco de Alimentos/Ads/View/Ads.swift
@@ -5,4 +5,37 @@
 //  Created by user192057 on 9/6/21.
 //
 
-import Foundation
+import SwiftUI
+import AVKit
+
+struct ContentView: View {
+    var body: some View {
+        
+        GeometryReader{geo in
+            player().frame(height: geo.size.height).cornerRadius(20).padding()
+        }
+    }
+}
+
+struct player: UIViewControllerRepresentable{
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<player>) -> AVPLayerViewController {
+        
+        let controller = AVPLayerViewController()
+        let player1 = AVPlayer(url: URL(string: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!)
+        controller.player = player1
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: AVPLayerViewController, context: UIViewControllerRepresentableContext<player>){
+        
+    }
+    
+}
+
+
+struct Ads_Previews: PreviewProvider {
+    static var previews: some View {
+        Ads()
+    }
+}
