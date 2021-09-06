ce3dad047c7c03d2dd0bea808ea036d7b0fd387a
DavidVelazquez584 <A01632648@itesm.mx>
2021-09-06 12:56:54 -0500

    ads already finished
    The other changes was made by me but in mac in cloud by the name user

--- Banco de Alimentos/Banco de Alimentos/Ads/View/Ads.swift
+++ Banco de Alimentos/Banco de Alimentos/Ads/View/Ads.swift
@@ -9,28 +9,67 @@ import SwiftUI
 import AVKit
 
 struct Ads: View {
+
+
     var body: some View {
+
+        @State var height : CGFloat = 0
+        @State var show = false
         
         GeometryReader{geo in
-            player().frame(height: geo.size.height / 3).cornerRadius(20).padding()
-        }
+
+            ZStack {
+                Button (action: {
+
+                  self.show.toggle()
+                  self.height = 0
+
+                }) {
+
+                    Text("Picture in Play")
+
+                }
+
+                if self.show {
+                    player().frame(height: geo.size.height / 3).cornerRadius(20).padding().gesture(DragGesture()
+                      .onChanged({ (value) in
+                          self.height = value.translation.height / 10
+                      })
+
+                      .onEnded({ (value) in
+                          if self.height > geo.size.height / 2 - 100 {
+                              self.height = 1500
+                              self.show.toggle()
+                          }
+                          if -self.height > geo.size.height / 2 - 100 {
+                              self.height = -1500
+                              self.show.toggle()
+                          }
+                      })
+                    ).offset(y: self.height)
+
+                }
+            }
+
+
+        }.animation(.sprint())
     }
 }
 
 struct player: UIViewControllerRepresentable{
-    
+
     func makeUIViewController(context: UIViewControllerRepresentableContext<player>) -> AVPlayerViewController {
-        
+
         let controller = AVPlayerViewController()
         let player1 = AVPlayer(url: URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!)
         controller.player = player1
         return controller
     }
-    
+
     func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<player>){
-        
+
     }
-    
+
 }
 
 
