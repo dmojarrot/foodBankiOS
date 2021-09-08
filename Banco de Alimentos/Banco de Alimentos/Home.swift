1f05da757e21092144ddad88d6f41c1eaa6cd1c2
Andrés Contreras <42359482+Andresmagic@users.noreply.github.com>
2021-09-07 20:23:33 -0700

    Updated Home.swift

--- Banco de Alimentos/Banco de Alimentos/Home.swift
+++ Banco de Alimentos/Banco de Alimentos/Home.swift
@@ -12,16 +12,63 @@ struct Home: View {
         NavigationView {
             ZStack {
                 Color("background").edgesIgnoringSafeArea(.bottom)
-
-                ScrollView {
-                    Color("background").edgesIgnoringSafeArea(.bottom)
-                    ScrollView{
-                    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                ScrollView{
+                    
+            
+                        ZStack(alignment: .leading) {
+                            Rectangle()
+                                .foregroundColor(Color.white)
+                                .cornerRadius(20.0)
+                                .shadow(color: .black, radius: 0.4, y: 0.2)
+                                
+                            VStack(alignment: .leading){
+                                Text("Descripcion").bold().font(.title2)
+                                    .padding(.bottom, 5)
+                                    .padding(.horizontal, 10)
+                                .padding(.top, 20)
+                                Text(" En esta aplicacion podras ser capaz de donar tanto comida como dinero al banco de comidas de guadalajara. Con tu apoyo podremos llegar mas comidas a los mas necesitados:)")
+                                    .padding(.horizontal, 20)
+                                .padding(.bottom, 30)
+                            }
+                        }
+                        .padding(.top, 60)
+                        .padding(.bottom, 60)
+                    ZStack(alignment: .leading) {
+                        Rectangle()
+                            .foregroundColor(Color.white)
+                            .cornerRadius(20.0)
+                            .shadow(color: .black, radius: 0.4, y: 0.2)
+                        VStack(alignment: .leading){
+                                Text("Donar").bold().font(.title2)
+                                    .padding(.bottom, 5)
+                                    .padding(.horizontal, 10)
+                                .padding(.top, 20)
+                                Text(" Puedes donar tanta comida como desees y tanto dinero como gustes, incluso si no tienes recursos para donar puedes simplemente ver los ADS y de esta manera donar directamente a nuestra causa")
+                                    .padding(.horizontal, 20)
+                                .padding(.bottom, 30)
+                            }
                     }
-                }.navigationBarTitle("Home")
-            }
+                    .padding(.bottom, 60)
+                        
+                        NavigationLink(
+                            destination: Acknowledgment(),
+                            label: {
+                                Label("Agradecimientos", systemImage: "person.crop.circle")
+                                    .foregroundColor(.blue)
+                                    .padding(20)
+                                    .multilineTextAlignment(.leading)
+                                    .frame(maxWidth: .infinity)
+                                    .background(RoundedRectangle(cornerRadius: 20)
+                                                    .fill(Color.white))
+                                    .padding(.horizontal, 20)
+                                
+                            })
+                        Spacer()
+                    }.padding(.horizontal, 20)
+                }.navigationTitle("Home")
+            
         }
-    }
+        }
 }
 
 struct Home_Previews: PreviewProvider {
@@ -29,3 +76,4 @@ struct Home_Previews: PreviewProvider {
         Home()
     }
 }
+
