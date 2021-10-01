e9f8c014c2df6f0ca9a7b67d47bd5bafaeac853c
dmojarrot <a01638460@itesm.mx>
2021-10-01 11:29:11 -0500

    minor things

--- Banco de Alimentos/Banco de Alimentos/Acknowledgment.swift
+++ Banco de Alimentos/Banco de Alimentos/Acknowledgment.swift
@@ -8,41 +8,41 @@
 import SwiftUI
 
 struct Acknowledgment: View {
-
+    
     var body: some View {
-            ZStack {
-                Rectangle().foregroundColor(.clear).background(LinearGradient(gradient: Gradient(colors: [.white, Color.gray]), startPoint: .bottom,  endPoint: .top).edgesIgnoringSafeArea(.all))
-                ScrollView{
+        ZStack {
+            Rectangle().foregroundColor(.clear).background(LinearGradient(gradient: Gradient(colors: [.white, Color.gray]), startPoint: .bottom,  endPoint: .top).edgesIgnoringSafeArea(.all))
+            ScrollView{
+                
+                VStack(alignment: .leading){
+                    Text("Muchas gracias por formar parte!")
+                        .bold()
+                        .padding(.top, 10)
+                        .padding(.leading, 10)
+                        .padding(.bottom)
+                        .font(.title3)
+                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                        .padding(30)
+                        .multilineTextAlignment(.leading)
+                        .frame(maxWidth: .infinity)
+                        .background(RoundedRectangle(cornerRadius: 20)
+                                        .fill(Color.white))
                     
-                    VStack(alignment: .leading){
-                        Text("Muchas gracias por formar parte!")
-                            .bold()
-                            .padding(.top, 10)
-                            .padding(.leading, 10)
-                            .padding(.bottom)
-                            .font(.title3)
-                        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
-                            .padding(30)
-                            .multilineTextAlignment(.leading)
-                            .frame(maxWidth: .infinity)
-                            .background(RoundedRectangle(cornerRadius: 20)
-                                            .fill(Color.white))
-                            .shadow(color: .black, radius: 0.4, y: 0.2)
-
-                            .padding(.bottom, 40)
-                        
-                        VideoView(videoURL: URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4")!, previewLength: 30)
-                            .cornerRadius(15)
-                            .frame(width: nil, height: 250, alignment: .center)
-                            .shadow(color: .black, radius: 0.4, y: 0.2)
-                            .padding(.bottom, 10)
-                        
-                        
-                        Spacer()
-                    }.padding(.horizontal, 20)
-                }.navigationTitle("Agradecimientos")
-            }
-            
+                    
+                        .padding(.bottom, 40)
+                    
+                    VideoView(videoURL: URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4")!, previewLength: 30)
+                        .cornerRadius(15)
+                        .frame(width: nil, height: 250, alignment: .center)
+                        .shadow(color: .black, radius: 0.4, y: 0.2)
+                        .padding(.bottom, 10)
+                    
+                    
+                    Spacer()
+                }.padding(.horizontal, 20)
+            }.navigationTitle("Agradecimientos")
+        }
+        
     }
 }
 
